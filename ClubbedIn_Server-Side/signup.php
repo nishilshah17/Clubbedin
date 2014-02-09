ead208a0a6a8385fbbf2b467421182dfaff92dac
nishilshah17 <nishil.shah17@gmail.com>
2014-02-09 16:20:25 -0500

    Removing database pws 2

--- ClubbedIn_Server-Side/signup.php
+++ ClubbedIn_Server-Side/signup.php
@@ -1,10 +1,7 @@
 <?php
 header("access-control-allow-origin: *");
 
-$dbhost = 'localhost';
-$dbuser = 'clubbed_admin';
-$dbpass = 'ClubbedIn2013';
-$db = 'clubbed_main';
+include 'mysqlconnect.php';
 
 $dbserver = mysql_connect($dbhost, $dbuser, $dbpass)
 	or die("Unable to connect to MySQL: " . mysql_error());

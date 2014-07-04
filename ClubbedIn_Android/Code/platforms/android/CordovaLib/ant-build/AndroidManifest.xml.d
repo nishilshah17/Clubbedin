4b41bbec6b559d7db1575cdd43d38af8c7364356
Rohfosho <rohvar98@gmail.com>
2014-07-04 11:37:16 -0400

    UI/UX redesign

---
+++ ClubbedIn_Android/Code/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Rohith\Documents\GitHub\ClubbedIn_Android\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Rohith\Documents\GitHub\ClubbedIn_Android\platforms\android\CordovaLib\AndroidManifest.xml \

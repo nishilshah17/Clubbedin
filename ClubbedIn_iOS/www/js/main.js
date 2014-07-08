ead570a4c798927e7d20a7692ef1563244495e50
nishilshah17 <nishil.shah17@gmail.com>
2014-07-08 16:58:11 -0400

    updated iOS

--- ClubbedIn_iOS/www/js/main.js
+++ ClubbedIn_iOS/www/js/main.js
@@ -2527,4 +2527,4 @@ function getMembersAfterSearch(all) {
     'warningNumber': 40,
     'displayFormat': '#left'
     };
-    //$('#textareapost').textareaCount(options2);
+    $('#textareapost').textareaCount(options2);
